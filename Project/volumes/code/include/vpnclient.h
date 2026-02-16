041546521aac50ada7e55fab000c7c0ed6b383b4
pjc0210 <75004958+pjc0210@users.noreply.github.com>
2026-02-16 16:19:36 -0500

    project

--- Project/volumes/code/include/vpnclient.h
+++ Project/volumes/code/include/vpnclient.h
@@ -27,13 +27,13 @@ void sock_callback(int tunfd, int sockfd, struct sockaddr_in *server);
 /**
  * Check the checksum of a WireChild packet.
  *
- * @param wc           The WireChild packet.
+ * @param pkt          The packet.
  * @param payload_len  The length of the payload.
  *
  * @return 1 if the checksum is valid, 0 otherwise.
  */
 int
-check_checksum(struct WireChild *wc, size_t payload_len);
+check_checksum(void *pkt, size_t payload_len);
 
 /**
  * Perform a handshake with the peer server
